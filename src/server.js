6188970f753444591ef305c1bad422ef013e4d3e
Roman Havryliuk <romanfest1221@gmail.com>
2026-05-10 20:51:04 +0200

    update express.json middleware configuration

--- src/server.js
+++ src/server.js
@@ -11,7 +11,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(logger);
-app.use(express.json());
+app.use(express.json(
+  {
+    type: ['application/json', 'application/vnd.api+json'],
+    limit: '100kb',
+  }
+));
 app.use(cors());
 
 app.use(notesRoutes);
